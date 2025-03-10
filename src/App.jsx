5f8ae6abcd1b988a0195a274e02cb2e0cfd4839c
Otutochi <o.caleb.nwa@gmail.com>
2025-03-09 19:03:56 -0500

    added-shuffle-button

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,55 @@ const App = () => {
 
   const getRandomIndex = () => Math.floor(Math.random() * (flashcards.length - 1));
 
-  const [index, setIndex] = useState(getRandomIndex())
+  const [flashCards, setFlashCards] = useState(flashcards);
 
-  const updateIndex = () => {
+  const shuffleCards = () => {
+    let shuffledCards = [...flashcards];
+    for(let i=flashcards.length -1 ; i > 0 ; i--){
+      const j = Math.floor(Math.random() * (i+1));
+      [shuffledCards[i], shuffledCards[j]] = [shuffledCards[j], shuffledCards[i]];
+    }
+
+    setFlashCards(shuffledCards);
+  }
+
+  const handleShuffle = () => {
+    shuffleCards();
+    setIndex(0);
+    setMode("event");
+
+  }
+
+  const [index, setIndex] = useState(0);
+
+  const [guess, setGuess] = useState("");
+
+  const [checkGuess, setCheckGuess] =useState("");
+
+  const handleGuess = (e) => {
+    setGuess(e.target.value);
+  }
+
+  const submitGuess = () => {
+    if(guess != flashCards[index].year){
+      setCheckGuess("wrong");
+    } else {
+      setCheckGuess("correct");
+    }
+  }
+
+  
+  const prevIndex = () => {
+    setIndex(index - 1); 
+    setMode("event");
+  }
+
+  const nextIndex = () => {
+    setIndex(index + 1); 
+    setMode("event");
+  }
+
+  const randomIndex = () => {
     setIndex(getRandomIndex()); 
     setMode("event");
   }
@@ -37,9 +83,16 @@ const App = () => {
       <p>Check if you can correctly guess the year each of these {flashcards.length} historical events happened</p>
       
 
-      <Card index={index} mode={mode} onClick={updateMode}/>
+      <Card currentFlashCard={flashCards[index]} mode={mode} onClick={updateMode}/>
 
-      {mode != "start" && <button onClick = {updateIndex} >Next</button>}
+      {mode != "start" && <input type="text" name="guess" value={guess} placeholder="Take a guess before flipping" onChange={handleGuess} className={checkGuess} />}
+      {mode != "start" && <button onClick={submitGuess} >Submit</button>}
+
+      {mode != "start" && index != 0 && <button onClick = {prevIndex} >Prev</button>}
+      {mode != "start" && index != flashCards.length -1 && <button onClick = {nextIndex} >Next</button>}
+      {mode != "start" && <button onClick = {randomIndex} >Random</button>}
+
+      {mode != "start" && <button onClick={handleShuffle}>Shuffle Cards</button>}
       
     </div>
   )
